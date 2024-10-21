aa33602dcddf05966ca144e9b2d18aeee4753d92
Pierre-marie Combalbert <cmp3321a@ensfsi.onmicrosoft.com>
2024-10-21 17:30:49 +0200

    création de la classe Humain

---
+++ src/personnages/Humain.java
@@ -0,0 +1,53 @@
+package personnages;
+
+public class Humain {
+	private String nom;
+	
+	private String boissonFavorite;
+	
+	private int argent;
+	
+	
+	public Humain(String nom, String boissonFavorite, int argent) {
+		this.nom = nom;
+		this.boissonFavorite = boissonFavorite;
+		this.argent = argent;
+	}
+
+	private void parler(String texte) {
+		System.out.println("« " + texte + "»");
+	}
+	
+	public String getNom() {
+		return nom;
+	}
+
+	public int getArgent() {
+		return argent;
+	}
+
+	public void direBonjour() {
+		this.parler("Bonjour ! Je m'appelle " + this.nom + " et j'aime boire du " + this.boissonFavorite);
+	}
+	
+	public void boire() {
+		this.parler("Mmmm, un bon verre de " + this.boissonFavorite + "! GLOUPS!");
+	}
+	
+	public void acheter(String bien, int prix) {
+		if(prix < argent) {
+			this.parler("J'ai " +  argent + " en poche. Je vais pouvoir m'offrir un/une " + bien +" à " + prix + " sous");
+			this.perdreArgent(prix);
+		} else {
+			this.parler("Je n'ai plus que " +  argent + " en poche. Je ne peux même pas m'offrir un/une " + bien + " à " + prix + " sous");
+		}
+	}
+	
+	public void gagnerArgent(int gain) {
+		this.argent += gain;
+	}
+	
+	public void perdreArgent(int perte) {
+		this.argent -= perte;
+	}
+}
